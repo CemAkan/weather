a963c834d78f0d73b01ff67aad22545b5453194b
CemAkan <cemakan5014@gmail.com>
2023-01-24 13:40:56 +0300

    getting location info from user (yargs) added

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var weather = require("./weather");
 var ip = require("./ip");
+var argv = require("yargs")
+.options("location", {
+    alias: "l",
+    demand: false,
+    describe: "Location info for weather info.",
+    type:"string"
+}).help("help").argv;
 
-ip(function(location){
-    weather(location, function (currentWeather) {
+
+
+if(typeof argv.l === "string" && argv.l.length > 0){
+    weather(argv.l, function (currentWeather) {
         console.log(currentWeather);
-    })
-});
+    });
+}
+else{
+    ip(function(location){
+        weather(location, function (currentWeather) {
+            console.log(currentWeather);
+        });
+    });
+}
