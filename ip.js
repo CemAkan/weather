2a011d6c87e81836cc11353f424ea0c20b3b78d5
CemAkan <cemakan5014@gmail.com>
2023-01-28 11:19:19 +0300

    reedit for dotenv

--- ip.js
+++ ip.js
@@ -1,19 +1,19 @@
 var request = require("request");
-var url="https://ipinfo.io?token=";
-var token="be08fcb77e2227";
-var finalUrl=url+token;
+require("dotenv").config();
+var url = "https://ipinfo.io?token=";
+var token = process.env.TOKEN;
+var finalUrl = url + token;
 
-module.exports =  function ip (){
-    return new Promise(function(resolve,reject){
-        request(finalUrl, (error, response, body)=>{
-    
-            if(error){
-              reject();
-            }else{
-                var data = JSON.parse(body);
-                var location = data.city;
-                resolve(location);
-            };
-        });
+module.exports = function ip() {
+  return new Promise(function (resolve, reject) {
+    request(finalUrl, (error, response, body) => {
+      if (error) {
+        reject();
+      } else {
+        var data = JSON.parse(body);
+        var location = data.city;
+        resolve(location);
+      }
     });
+  });
 };
--- weather.js
+++ weather.js
@@ -1,31 +1,30 @@
 var request = require("request");
-var key = "a06972ab30064f14b05131806232301";
 var url = "http://api.weatherapi.com/v1/current.json?key=";
 
+require("dotenv").config();
+var key = process.env.KEY;
 
-module.exports =  function(location, callback){
-   return new Promise(function(resolve,reject){
-    var fixedLocation =encodeURIComponent(location); // boşlukları %20 ile değiştirir.
+module.exports = function (location, callback) {
+  return new Promise(function (resolve, reject) {
+    var fixedLocation = encodeURIComponent(location); // Kullanıcıdan alınan veride boşluk varsa boşlukları " %20 " ile değiştirir.
 
-    if(!location){
-       return reject("Location can't foud");
+    if (!location) {
+      return reject("Location can't foud");
+    } else {
+      var finalUrl = url + key + "&q=" + fixedLocation;
+
+      request(finalUrl, (error, response, body) => {
+        if (error) reject("Weather infos couldn't find.");
+
+        try {
+          var data = JSON.parse(body);
+          resolve(
+            `In ${data.location.name} temperature is ${data.current.temp_c} Celcius.`
+          );
+        } catch {
+          reject("Please write real city name.");
+        }
+      });
     }
-    else{
-        var finalUrl = url + key + "&q=" + fixedLocation;
-
-        request(finalUrl, (error, response, body)=>{
-        if(error) reject("Weather infos couldn't find.");
-
-        try{
-            var data = JSON.parse(body);
-            resolve(`In ${data.location.name} temperature is ${data.current.temp_c} Celcius.`);
-        }
-        catch{
-            reject("Please write real city name.");
-        }
-       });
-     };
-   })
+  });
 };
-
-
